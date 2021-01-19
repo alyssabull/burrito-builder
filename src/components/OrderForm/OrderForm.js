065fb2bc403b8d4474b32117ae4ea4a737e7bfa8
Alyssa Bull <alyssabull@gmail.com>
2021-01-19 09:33:13 -0700

    Remove preventDefault so order loads on DOM on submit click

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -26,7 +26,6 @@ class OrderForm extends Component {
   }
 
   handleSubmit = e => {
-    e.preventDefault();
     if (this.state.name !== '' && this.state.ingredients.length > 0) {
       this.props.updateOrders(this.state.name, this.state.ingredients)
     }
